af461595edc9dea239a49349ae0d8049061fd1c1
Vesna Petkovic <vesna.5kovic@gmail.com>
2020-07-15 20:32:07 +0200

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
    $massage = $_POST['massage'];
 
 
-   $mailTo = "vesna.5kovic@gmail.com";
+   $mailTo = "vesna.86@yahoocom";
    $headers = "From: ".$mailFrom;
    $txt = "Email od ".$name.".\n\n".$massage;
 
